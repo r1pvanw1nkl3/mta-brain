52639c338439f1da020154212aeda1ede4e9c7a7
Chris Stone <chrissstone@gmail.com>
2026-01-19 12:57:42 -0500

    start building models

---
+++ src/transit_core/core/models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, ConfigDict
+
+class Station(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    stop_id: str
+    stop_name: str
+    lat: float
+    lon: float
+    parent_station: str
